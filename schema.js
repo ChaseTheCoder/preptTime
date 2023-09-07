db89c3c94af840d7b78c83996e1f8bb67321667e
Chase The Coder <65514507+ChaseTheCoder@users.noreply.github.com>
2023-09-06 21:04:51 -0500

    Create schema.js

---
+++ schema.js
@@ -0,0 +1,85 @@
+{
+	"_id": "12SCOPE",
+	"authors": [
+		ObjectID("1TEACHER"),
+		ObjectID("2TEACHER")
+	],
+	"units": [
+		ObjectID("1UNIT"),
+		ObjectID("2UNIT"),
+		ObjectID("3UNIT"),
+	]
+}
+
+{
+	"_id": "1UNIT",
+	"grade": "2",
+	"subject": "Math",
+	"unitObjectives": "Measure things",
+	"essentialQuestions: [
+		{
+			"question": "How do we measure with and without measuring tools?"
+		}
+	],
+	"resources": [
+		{
+			"link": "www.linktodocument.com/math"
+		},
+				{
+			"link": "www.linktodocument.com/math2"
+		}
+	],
+	"description": "In this 12-day Grade 2 module, students engage in activities designed to deepen their conceptual understanding of measurement and to relate addition and subtraction to length. Their work in Module 2 is exclusively with metric units in order to support place value concepts. Customary units are introduced in Module 7."
+	"standards": [
+		ObjectID("1STAND"),
+		ObjectID("2STAND"),
+		ObjectID("3STAND"),
+		ObjectID("4STAND")
+	]
+}
+
+{
+	"_id": "1STAND",
+	"standard": "Represent and solve problems involving addition and subtraction.",
+	"number": "CCSS.MATH.CONTENT.2.OA.A.1",
+	"description": "Use addition and subtraction within 100 to solve one- and two-step word problems involving situations of adding to, taking from, putting together, taking apart, and comparing, with unknowns in all positions, e.g., by using drawings and equations with a symbol for the unknown number to represent the problem."
+	"lessons": [
+		ObjectID("1LESSON"),
+		ObjectID("2LESSON"),
+		ObjectID("3LESSON"),
+		ObjectID("4LESSON")
+	]
+}
+
+{
+	"_id": "1LESSON",
+	"objective": "Practice making ten and adding to ten",
+	"materailsAndReminders": [
+		{
+			"title": "Exit Ticket",
+			"link": "www.linktoexitticket.com/1"
+		}
+	],
+	"activities": [
+		{
+			"activity": "opening",
+			"description": "Ask students something about a group of ten things."
+		},
+		{
+			"activity": "lesson",
+			"description": "Draw a group of ten on the board."
+		}
+	],
+	"assessment": [
+		{
+			"title": "Exit Ticket",
+			"link" :
+		}
+	]
+	"notes": "Here are my notes for the lesson"
+}
+
+{
+	"_id": "1CONTENT",
+
+}
